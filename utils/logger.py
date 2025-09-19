b29188e22b954d22928714dd6aa03a0f459aba43
redox121223233 <kasraehs117@gmail.com>
2025-09-19 23:43:08 +0330

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s:%(message)s"
+    format="%(asctime)s %(levelname)s:%(message)s",
 )
+
 logger = logging.getLogger("sticker_bot")
