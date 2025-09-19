c88f5e2289bd3ba99e54ef2754e28ca49a58ec12
redox121223233 <kasraehs117@gmail.com>
2025-09-19 20:38:49 +0330

    Delete utils directory

--- utils/keyboards.py
+++
@@ -1,9 +0,0 @@
-def main_menu_markup():
-    return {"keyboard":[[{"text":"🎁 تست رایگان"},{"text":"⭐ اشتراک"}],[{"text":"🎭 استیکرساز"}]], "resize_keyboard":True}
-
-def welcome_inline_markup():
-    keyboard = [
-        [{"text":"✨ ساخت استیکر جدید","callback_data":"new_sticker"},{"text":"📚 قالب‌های آماده","callback_data":"show_templates"}],
-        [{"text":"💎 خرید اشتراک","callback_data":"show_subscription"},{"text":"🎁 تست رایگان","callback_data":"show_free_trial"}]
-    ]
-    return {"inline_keyboard": keyboard}
