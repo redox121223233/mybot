d000cfc152d15b95882e09183f3f8fe1c43406d5
redox121223233 <kasraehs117@gmail.com>
2025-09-19 23:50:36 +0330

    Update legacy.py

--- services/legacy.py
+++ services/legacy.py
@@ -1,20 +1,15 @@
-from config import BOT_TOKEN, BASE_DIR
-from utils.telegram_api import send_message
+
+from config import BOT_TOKEN, DATA_DIR as BASE_DIR
+from utils.telegram_api import TelegramAPI
 from services.database_manager import DatabaseManager
 from services.subscription_manager import SubscriptionManager
 from services.menu_manager import MenuManager
 from services.sticker_manager import StickerManager
 from services.ai_manager import AIManager
 
-class TelegramAPI:
-    def __init__(self, token):
-        self.token = token
-
 api = TelegramAPI(BOT_TOKEN)
-
-# سرویس‌ها
 db_manager = DatabaseManager(BASE_DIR)
 subscription_manager = SubscriptionManager(db_manager, "subscriptions.json")
 menu_manager = MenuManager(api, BOT_TOKEN)
-sticker_manager = StickerManager(api, db_manager)
+sticker_manager = StickerManager(api, db_manager, BASE_DIR)
 ai_manager = AIManager(api)
