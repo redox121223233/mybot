94480181f335274ef0764e95ccfae246a2e4cf0e
redox121223233 <kasraehs117@gmail.com>
2025-09-21 21:26:26 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"
 DOMAIN = "https://mybot-production-61d8.up.railway.app"
 
 # 📢 نام کاربری کانال (بدون لینک کامل)
-CHANNEL_USERNAME = "@redoxBOT_STICKER"
+CHANNEL_USERNAME = "redoxBOT_STICKER"
 
 # 🌍 لینک وبهوک
 WEBHOOK_URL = f"{DOMAIN}/webhook/{BOT_TOKEN}"
+
