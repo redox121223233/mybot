e56dc12a85363f2e7ff0535b8ddb874245d96e66
redox121223233 <kasraehs117@gmail.com>
2025-09-21 22:39:13 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 
 # ======= تنظیمات اصلی =======
 BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"   # <-- اینجا توکن ربات رو بذار
-CHANNEL_USERNAME = "redoxbot_sticker"  # مثلاً "redoxBOT_STICKER" (بدون @ یا با @ هر دو پشتیبانی میشه)
+CHANNEL_USERNAME = "@redoxbot_sticker"  # مثلاً "redoxBOT_STICKER" (بدون @ یا با @ هر دو پشتیبانی میشه)
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")  # مسیر ذخیره‌سازی محلی
 FONTS_DIR = os.path.join(DATA_DIR, "fonts")
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(FONTS_DIR, exist_ok=True)
+
