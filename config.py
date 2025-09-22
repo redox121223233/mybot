bb62a6f7471f52fba94e8b9f48f7222e8462cb8f
redox121223233 <kasraehs117@gmail.com>
2025-09-22 19:04:22 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # config.py
 import os
 
-BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"
-API = f"https://api.telegram.org/bot{BOT_TOKEN}/"
+# ======= تنظیمات اصلی =======
+BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"   # توکن ربات
+CHANNEL_USERNAME = "@redoxbot_sticker"  # یوزرنیم کانال عمومی (با @)
 
-# لینک کانال اجباری
-CHANNEL_LINK = "@redoxbot_sticker"  # هم با @ هم بدون @ پشتیبانی میشه
-
-# مسیر دیتا
+# مسیر ذخیره‌سازی محلی
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 FONTS_DIR = os.path.join(DATA_DIR, "fonts")
 os.makedirs(DATA_DIR, exist_ok=True)
