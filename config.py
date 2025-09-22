4f0c0009a2b5134ef85da6fcb41b55569683bed2
redox121223233 <kasraehs117@gmail.com>
2025-09-22 13:41:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
-# config.py
 import os
 
 # ======= تنظیمات اصلی =======
-BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"   # <-- اینجا توکن ربات رو بذار
-CHANNEL_USERNAME = "@redoxbot_sticker"  # مثلاً "redoxBOT_STICKER" (بدون @ یا با @ هر دو پشتیبانی میشه)
-DATA_DIR = os.path.join(os.path.dirname(__file__), "data")  # مسیر ذخیره‌سازی محلی
+BOT_TOKEN = "8324626018:AAEiEd_zcpuw10s1nIWr5bryj1yyZDX0yl0"   # توکن واقعی ربات
+CHANNEL_USERNAME = "redoxbot_sticker"  # بدون @ بزن
+
+# ======= تنظیمات وبهوک =======
+DOMAIN = "https://mybot-production-61d8.up.railway.app"  # دامین Railway
+WEBHOOK_URL = f"{DOMAIN}/webhook/{BOT_TOKEN}"
+
+# ======= مسیرهای ذخیره‌سازی =======
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 FONTS_DIR = os.path.join(DATA_DIR, "fonts")
+
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(FONTS_DIR, exist_ok=True)
-
